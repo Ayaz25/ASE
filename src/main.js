2612fe744cf12744a51ee0daf1be45f38781a611
Ayaz25 <ayazhasan@web.de>
2024-12-08 20:36:23 +0100

    finish

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "@/components/HomePage.vue";
+import StartSite from "@/components/StartSite.vue";
+import ToastService from "primevue/toastservice";
+import './registerServiceWorker'
 
-createApp(App).mount('#app')
+const router =  createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/startsite'
+        },
+        {
+            path: '/homepage/:userName',
+            name: 'homepage',
+            component: HomePage,
+        },
+        {
+            path: '/startsite',
+            name: 'startsite',
+            component: StartSite
+        }
+    ]
+})
+
+createApp(App).use(router).use(ToastService).use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+}).mount('#app')
+
+export default router;
